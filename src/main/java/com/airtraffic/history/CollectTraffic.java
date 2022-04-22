25f8d7f14e07451b43baac3fff3cb45e98ab3f89
Alexander.Aghili <alexander.w.aghili@gmail.com>
2022-04-21 23:50:37 -0700

    Added MongoDB, revamp architecture incoming

--- src/main/java/com/airtraffic/history/CollectTraffic.java
+++
@@ -1,6 +0,0 @@
-package com.airtraffic.history;
-
-public class CollectTraffic 
-{
-	
-}
---
+++ src/main/java/com/airtraffic/history/database/DatabaseConnectorBuilder.java
@@ -0,0 +1,12 @@
+package com.airtraffic.history.database;
+
+
+//TODO: Build Builder architecture
+public class DatabaseConnectorBuilder 
+{
+	private static boolean isReadWrite;
+	
+	public static class DatabaseConnector {
+		
+	}
+}
